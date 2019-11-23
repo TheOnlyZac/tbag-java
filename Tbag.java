d497ab13787b721525a8f9585ebd5539ad3165c1
Zac Krasnow <TheOnlyZac@gmail.com>
2019-11-23 01:55:58 -0500

    echo input back to console

--- Tbag.java
+++ Tbag.java
@@ -1,7 +1,16 @@
+import java.util.Scanner;
+
 class Tbag {
     public static void main(String[] args)
     {
+        Scanner input = new Scanner(System.in);
+
         BaseObject debugSword = new BaseObject("Debug Sword");
         System.out.println("Initialized sword as '" + debugSword.name + "'");
+
+        while(true) {
+            String txt = input.nextLine();
+            System.out.println(txt);
+        }
     }
 }
