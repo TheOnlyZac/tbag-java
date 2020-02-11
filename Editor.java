32155c546dae6f38c75582969f0ecbdaded397e0
Zac Krasnow <TheOnlyZac@gmail.com>
2020-02-11 16:43:20 -0500

    organized editor code

--- Editor.java
+++ Editor.java
@@ -5,18 +5,41 @@ import java.awt.event.ActionListener;
 
 class Editor {
 	
+	private static JFrame frame;
+	
 	public static void main(String args[])
 	{
 
 		// Create JFrame
-		JFrame frame = new JFrame("TBAG GUI");
+		frame = new JFrame("TBAG GUI");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setSize(600, 600);
 
-		// Generate menu bar
-		JMenuBar menuBar = new JMenuBar();
+		CreateMenuBar();
+
+		// Item list
+		DefaultListModel<BaseObject> objListModel = new DefaultListModel<BaseObject>();
+		JList<BaseObject> objList = new JList<BaseObject>(objListModel);
+		objListModel.addElement(new BaseObject("Test Object", "A test object"));
+		objListModel.addElement(new Item("Test Item", "A fun item"));
+
+
+		JPanel cardsPanel = new JPanel(new CardLayout());
+		cardsPanel.add(objList);
+		frame.getContentPane().add(cardsPanel);
+
+		frame.setVisible(true);
+	}
+	
+	static void CreateMenuBar()
+	{
+		/**
+		 * Create the menubar on the editor window
+		 */
 		
-		//// File Menu
+		JMenuBar menuBar = new JMenuBar();
+
+		// File Menu
 		JMenu fileMenu = new JMenu("File");
 
 		JMenuItem mOpen = new JMenuItem("Open");
@@ -32,7 +55,9 @@ class Editor {
 		mExit.addActionListener((event) -> System.exit(0));
 		fileMenu.add(mExit);
 
-		//// Help Menu
+		menuBar.add(fileMenu);
+		
+		// Help Menu
 		JMenu helpMenu = new JMenu("Help");
 
 		JMenuItem mAbout = new JMenuItem("About");
@@ -43,30 +68,18 @@ class Editor {
 		});
 		helpMenu.add(mAbout);
 
-		menuBar.add(fileMenu);
 		menuBar.add(helpMenu);
-
-		// Item list
-		DefaultListModel<BaseObject> objListModel = new DefaultListModel<BaseObject>();
-		JList<BaseObject> objList = new JList<BaseObject>(objListModel);
-		objListModel.addElement(new BaseObject("Test Object", "A test object"));
-		objListModel.addElement(new Item("Test Item", "A fun item"));
 
 		// Add elements to main JFrame and set visibility
 		frame.getContentPane().add(BorderLayout.NORTH, menuBar);
-
-		JPanel cardsPanel = new JPanel(new CardLayout());
-		cardsPanel.add(objList);
-		frame.getContentPane().add(cardsPanel);
-
-		frame.setVisible(true);
 	}
 
-	static void AboutPopup() {
+	static void AboutPopup()
+	{
 		String html = "<html><body width='%1s'><H1>TBAG: Text Based Adventure Game</h1>"
 		+ "<p>A Java framework for creating text-based interactive stories.<br><br>"
 		+ "<p>Created by Zac Krasnow.";
-		int w = 400;
+		int w = 300;
 
 		JOptionPane.showMessageDialog(null, String.format(html, w, w));
 	}
